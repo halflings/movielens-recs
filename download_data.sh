f490aee40ebb1eb616fda417b216e9159a765d2a
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2017-04-14 19:09:19 +0100

    Small change to the unzipping command.
    Avoids redundant folders.

--- download_data.sh
+++ download_data.sh
@@ -1,14 +1,14 @@
 DATA_DIR="data"
-SMALL_DATASET_DIR="$DATA_DIR/ml-latest-small"
-LARGE_DATASET_DIR="$DATA_DIR/ml-latest"
+SMALL_DATASET_PATH="$DATA_DIR/ml-latest-small.zip"
+LARGE_DATASET_PATH="$DATA_DIR/ml-latest.zip"
 
 mkdir -p $DATA_DIR
 
 echo ". Downloading movielens-latest-small..."
-curl -o "$SMALL_DATASET_DIR.zip" http://files.grouplens.org/datasets/movielens/ml-latest-small.zip
+curl -o $SMALL_DATASET_PATH http://files.grouplens.org/datasets/movielens/ml-latest-small.zip
 echo ". Downloading movielens-latest..."
-curl -o "$LARGE_DATASET_DIR.zip" http://files.grouplens.org/datasets/movielens/ml-latest.zip
+curl -o $LARGE_DATASET_PATH http://files.grouplens.org/datasets/movielens/ml-latest.zip
 
 echo ". Unzipping data..."
-unzip "$SMALL_DATASET_DIR.zip" -d $SMALL_DATASET_DIR
-unzip "$LARGE_DATASET_DIR.zip" -d $LARGE_DATASET_DIR
+unzip $SMALL_DATASET_PATH -d $DATA_DIR
+unzip $LARGE_DATASET_PATH -d $DATA_DIR
